807dd2268f01b155c3392c82800df1f9d6ef8892
xionkq <3030199850@qq.com>
2023-09-25 21:18:41 +0800

    use preload

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
-
+    win.webContents.openDevTools()
     win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
+    console.log(process.versions)
 })
